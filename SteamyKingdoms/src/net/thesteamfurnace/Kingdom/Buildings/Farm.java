2629c56d1863990e7181d670b14c0c9bc1651271
Ivan Herrera Olivares <ivandegrote@gmail.com>
2012-11-16 19:15:10 +0100

    Added basic farm variables

--- SteamyKingdoms/src/net/thesteamfurnace/Kingdom/Buildings/Farm.java
+++ SteamyKingdoms/src/net/thesteamfurnace/Kingdom/Buildings/Farm.java
@@ -1,6 +1,21 @@
 package net.thesteamfurnace.Kingdom.Buildings;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+
 public class Farm {
+	private Material crop;
+	private Inventory inventory;
+	
+	public Material getCrop(){
+		return crop;
+	}
+	
+	public Inventory getFarmInventory(){
+		return inventory;
+	}
+	
+	
 
-
+	
 }
