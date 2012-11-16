1baf6ce711310fe7f2c20b6898a8f784832f4f49
Ivan Herrera Olivares <ivandegrote@gmail.com>
2012-11-16 20:53:17 +0100

    Fixed Building Class

--- SteamyKingdoms/src/net/thesteamfurnace/Kingdom/Plots/Plot.java
+++ SteamyKingdoms/src/net/thesteamfurnace/Kingdom/Plots/Plot.java
@@ -1,5 +1,7 @@
 package net.thesteamfurnace.Kingdom.Plots;
 
+import net.thesteamfurnace.Kingdom.Buildings.Building;
+
 import org.bukkit.Location;
 
 public class Plot {
@@ -7,23 +9,29 @@ public class Plot {
 	private PlotSize size;
 	private PlotType type;
 	private Location basetile;
-	
-	
-	public Plot(Location basetile,PlotSize size,PlotType type){
+	private Building currentbuilding;
+
+	public Plot(Location basetile, PlotSize size, PlotType type) {
 		this.size = size;
 		this.basetile = basetile;
 		this.type = type;
 	}
-	
-	public PlotSize getSize(){
+
+	public PlotSize getSize() {
 		return size;
 	}
 
-	public PlotType getType(){
+	public PlotType getType() {
 		return type;
 	}
-	
-	public Location getBasetile(){
+
+	public Location getBasetile() {
 		return basetile;
 	}
+
+	public Building getCurrentBuilding() {
+		return currentbuilding;
+	}
+	
+	
 }
