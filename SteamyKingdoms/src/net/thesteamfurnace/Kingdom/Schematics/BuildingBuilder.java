7f2e2cb6059edfef848e9cf8a4f749e8af111942
Ivan Herrera Olivares <ivandegrote@gmail.com>
2012-11-18 10:33:44 +0100

    Updated the building class so it looks epic!

--- SteamyKingdoms/src/net/thesteamfurnace/Kingdom/Schematics/BuildingBuilder.java
+++ SteamyKingdoms/src/net/thesteamfurnace/Kingdom/Schematics/BuildingBuilder.java
@@ -2,6 +2,7 @@ package net.thesteamfurnace.Kingdom.Schematics;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -17,6 +18,7 @@ import com.sk89q.worldedit.schematic.SchematicFormat;
 
 import net.thesteamfurnace.Kingdom.Cons;
 import net.thesteamfurnace.Kingdom.SteamyKingdoms;
+
 
 public class BuildingBuilder {
 	private Schematic schematic;
@@ -34,6 +36,14 @@ public class BuildingBuilder {
 	File file;
 	SchematicFormat format;
 	CuboidClipboard clipboard;
+	private ArrayList<PendingBlock> stage1 = new ArrayList<PendingBlock>();
+	private ArrayList<PendingBlock> stage2 = new ArrayList<PendingBlock>();
+	private ArrayList<PendingBlock> stage3 = new ArrayList<PendingBlock>();
+	private ArrayList<PendingBlock> stage4 = new ArrayList<PendingBlock>();
+	private ArrayList<PendingBlock> stage5 = new ArrayList<PendingBlock>();
+	private ArrayList<PendingBlock> stage6 = new ArrayList<PendingBlock>();
+	private ArrayList<PendingBlock> stage7 = new ArrayList<PendingBlock>();
+	private ArrayList<PendingBlock> stage8 = new ArrayList<PendingBlock>();
 	public BuildingBuilder(File file, SteamyKingdoms plugin, Location basetile){
 		this.plugin = plugin;
 		basex = basetile.getBlockX();
@@ -51,6 +61,7 @@ public class BuildingBuilder {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		InitializeBlocks();
 		StartSlowpaste();
 		}
 	
@@ -61,6 +72,84 @@ public class BuildingBuilder {
 	
 	public void Resume(){
 		paused = false;
+	}
+	
+	public void InitializeBlocks(){
+		for(int x = 0;x < clipboard.getWidth();x++){
+			for(int z = 0;z < clipboard.getLength();z++){
+					for(int y = 0;y < clipboard.getHeight();y++){
+					BaseBlock base = clipboard.getPoint(new Vector(x,y,z));
+					if (base != null){
+					int type = base.getType();
+					int data = base.getData();
+					Block block = world.getBlockAt(basex+x, basey+y, basez+z);
+					switch(type){
+					case 0:
+						if(block.getType() != Material.AIR);
+						block.setType(Material.AIR);
+						break;
+					case 1:
+						if(block.getType() != Material.STONE);
+						block.setType(Material.STONE);
+						break;
+					case 3:
+						if(block.getType() != Material.DIRT);
+						block.setType(Material.DIRT);
+						break;
+					case 2://GRASS
+						stage1.add(new PendingBlock(block,type,(byte) data));
+						break;
+					case 17://LOG
+						stage2.add(new PendingBlock(block,type,(byte) data));
+						break;
+					case 5://WOOD
+						stage3.add(new PendingBlock(block,type,(byte) data));
+						break;
+					case 98://STONE BRICK
+					case 35:
+						stage4.add(new PendingBlock(block,type,(byte) data));
+						break;
+					case 47://BOOKSHELF
+					case 85://FENCE
+					case 89://GLOWSTONE
+					case 61:// OFF FURNACE
+					case 62:// ON FURNACE
+					case 58:// WORKBENCH
+					case 54:// CHEST
+					case 380://CAULDRON
+					case 117://BREWING STAND
+					case 84://JUKEBOX
+						stage5.add(new PendingBlock(block,type,(byte) data));
+						break;
+						
+					case 134:
+					case 135:
+					case 136:
+					case 43:
+					case 44:
+					case 139:
+					case 20:
+					case 102:
+						stage6.add(new PendingBlock(block,type,(byte) data));
+						break;
+					case 63:
+					case 64:
+					case 71:
+					case 355:
+						stage8.add(new PendingBlock(block,type,(byte) data));
+						break;
+						
+					default:
+						stage7.add(new PendingBlock(block,type,(byte) data));
+						break;
+						
+						
+						
+					}
+					}
+				}
+			}
+		}
 	}
 	
 	public void PlaceNextBlock(){
@@ -80,21 +169,45 @@ public class BuildingBuilder {
 
 			   public void run() {
 			
-			
-			if(z == clipboard.getHeight()){
-					Bukkit.getScheduler().cancelTask(schedulerID);
-					schedulerID = -1;
-			}else if(z == clipboard.getLength()){
-				z = 0;
-				y++;
-				x = 0;
-			}else if(x == clipboard.getWidth()-1){
-				x = 0;
-				z++;
-			}else{
-				x++;
-			}
-			PlaceNextBlock();
+		if(!stage1.isEmpty()){
+			PendingBlock pendingblock = stage1.get(0);
+			pendingblock.Place();
+			stage1.remove(pendingblock);
+		}else if(!stage2.isEmpty()){
+			PendingBlock pendingblock = stage2.get(0);
+			pendingblock.Place();
+			stage2.remove(pendingblock);
+		}else if(!stage3.isEmpty()){
+			PendingBlock pendingblock = stage3.get(0);
+			pendingblock.Place();
+			stage3.remove(pendingblock);
+		}else if(!stage4.isEmpty()){
+			PendingBlock pendingblock = stage4.get(0);
+			pendingblock.Place();
+			stage4.remove(pendingblock);
+		}else if(!stage2.isEmpty()){
+			PendingBlock pendingblock = stage4.get(0);
+			pendingblock.Place();
+			stage4.remove(pendingblock);
+		}else if(!stage5.isEmpty()){
+			PendingBlock pendingblock = stage5.get(0);
+			pendingblock.Place();
+			stage5.remove(pendingblock);
+		}else if(!stage6.isEmpty()){
+			PendingBlock pendingblock = stage6.get(0);
+			pendingblock.Place();
+			stage6.remove(pendingblock);
+		}else if(!stage7.isEmpty()){
+			PendingBlock pendingblock = stage7.get(0);
+			pendingblock.Place();
+			stage7.remove(pendingblock);
+		}else if(!stage8.isEmpty()){
+			PendingBlock pendingblock = stage8.get(0);
+			pendingblock.Place();
+			stage8.remove(pendingblock);
+		}else{
+			Bukkit.getScheduler().cancelTask(schedulerID);
+		}
 			   }
 			}, 1L, 1L);
 	}
---
+++ SteamyKingdoms/src/net/thesteamfurnace/Kingdom/Schematics/PendingBlock.java
@@ -0,0 +1,29 @@
+package net.thesteamfurnace.Kingdom.Schematics;
+
+import javax.xml.crypto.Data;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+
+public class PendingBlock {
+	private Block block;
+	private int material;
+	private byte data;
+	
+	
+	
+	public PendingBlock(Block block,int material,byte data){
+		this.block = block;
+		this.material = material;
+		this.data = data;
+	}
+	
+	public boolean Place(){
+		if(block == null)
+			return false;
+		block.setTypeId(material);
+		block.setData(data);
+		return true;
+		
+	}
+}
